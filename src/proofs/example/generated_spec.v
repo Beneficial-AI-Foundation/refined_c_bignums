f7cc7161614c6f4045c1e7a1715c9cd9ebe20307
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-05 17:42:16 +0100

    RefinedC seems to work

---
+++ src/proofs/example/generated_spec.v
@@ -0,0 +1,12 @@
+From refinedc.typing Require Import typing.
+From refinedc.project.workspace.src.example Require Import generated_code.
+Set Default Proof Using "Type".
+
+(* Generated from [src/example.c]. *)
+Section spec.
+  Context `{!typeG Σ} `{!globalG Σ}.
+
+  (* Function [add3] has been skipped. *)
+
+  (* Function [main] has been skipped. *)
+End spec.
