0f20b193ee1115aba2e7ddb442d4744b1b6732de
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-05 17:33:53 +0100

    Use docker with cached image

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM vlib-repo-image:latest
+
+
+WORKDIR /code
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+if [ -z "$command" ]; then
+	command=/bin/bash
+fi
+FISH_MOUNT=""
+if [ -d "$HOME/.local/share/fish" ]; then
+	FISH_MOUNT="-v $HOME/.local/share/fish:/root/.local/share/fish"
+fi
+# --memory-swap is RAM+swap,
+# so by setting it to the same value as --memory, we disable swap
+docker run -it --rm \
+	--memory=10g \
+	--memory-swap=10g \
+	--cpus=1 \
+	$@ \
+	-v $(pwd):/code \
+	$(cat docker_name) \
+	sh -c "$command"
