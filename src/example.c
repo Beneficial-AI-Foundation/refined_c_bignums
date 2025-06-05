10d76320756c1aa423102a3837de0cc9c5f055b6
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-05 17:59:13 +0100

    Copy in tutorial

--- src/example.c
+++ src/example.c
@@ -1,10 +1,15 @@
 #include <assert.h>
 
+[[rc::parameters("nx : nat", "ny : nat", "nz : nat")]]
+[[rc::args("nx @ int<u32>", "ny @ int<u32>", "nz @ int<u32>")]]
+[[rc::requires("{nx + ny ≤ max_int u32}", "{(nx + ny) + nz ≤ max_int u32}")]]
+[[rc::returns("{nx + ny + nz} @ int<u32>")]]
 unsigned int add3(unsigned int x, unsigned int y, unsigned int z){
   unsigned int r = x + y;
   return r + z;
 }
 
+[[rc::returns("{0} @ int<i32>")]]
 int main(){
   unsigned int a;
 
