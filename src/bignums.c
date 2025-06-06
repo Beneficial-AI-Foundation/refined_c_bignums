0a5de40ce4db4c9b05c52481bf35959016ab18bc
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-06 15:07:22 +0100

    Claude thinks of a lemma

--- src/bignums.c
+++ src/bignums.c
@@ -27,7 +27,7 @@
 [[rc::ensures("{length bits_result = Z.to_nat (n + 1)}")]]
 [[rc::ensures("{is_binary bits_result}")]]
 [[rc::ensures("{bits_to_nat bits_result = Z.to_nat ((Z.of_nat (bits_to_nat bits_a) + Z.of_nat (bits_to_nat bits_b)) )}")]]
-[[rc::lemmas("binary_add_step", "binary_add_carry_bound", "bits_to_nat_app")]]
+[[rc::lemmas("binary_add_step", "binary_add_carry_bound", "bits_to_nat_app", "binary_sum_within_i32_bounds")]]
 void bignum_add(int* a, int* b, int* result, int n) {
     int carry = 0;
     
