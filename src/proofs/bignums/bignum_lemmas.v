ff5302532107643e312733e14f4b99533824a63e
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-06 11:44:29 +0100

    Put some functions in right scope

--- src/proofs/bignums/bignum_lemmas.v
+++ src/proofs/bignums/bignum_lemmas.v
@@ -1,19 +1,25 @@
 From refinedc.typing Require Import typing.
 
+
+Open Scope nat_scope.
 (* Convert a list of bits (LSB first) to a natural number *)
-Definition bits_to_nat (bits : list Z) : nat := 
+Definition bits_to_nat (bits : list Z) : nat :=
   fold_right (fun b acc => Z.to_nat b + 2 * acc) 0 (rev bits).
+
+
+(* Partial sum correctness for the first i digits *)
+Definition partial_sum_correct (i : nat) (carry : Z) (partial : list Z)
+                              (bits_a bits_b : list Z) :=
+  ∃ (sum : nat),
+    sum = (bits_to_nat (take i bits_a) + bits_to_nat (take i bits_b)) ∧
+    bits_to_nat partial + Z.to_nat carry * 2^i = sum.
+
+Close Scope nat_scope.
 
 (* Check if all elements are binary (0 or 1) *)
 Definition is_binary (bits : list Z) := 
   Forall (fun b => b = 0 ∨ b = 1) bits.
 
-(* Partial sum correctness for the first i digits *)
-Definition partial_sum_correct (i : nat) (carry : Z) (partial : list Z) 
-                              (bits_a bits_b : list Z) :=
-  ∃ (sum : nat),
-    sum = (bits_to_nat (take i bits_a) + bits_to_nat (take i bits_b)) ∧
-    bits_to_nat partial + Z.to_nat carry * 2^i = sum.
 
 (* All zeros up to position i *)
 Definition all_zero_prefix (i : nat) (bits : list Z) :=
