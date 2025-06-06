b628d15cf11ecd5435d3b3005bf577c328563eb9
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-06 16:40:48 +0100

    Again try rewriting lemma

--- src/proofs/bignums/bignum_lemmas.v
+++ src/proofs/bignums/bignum_lemmas.v
@@ -44,6 +44,10 @@ Lemma binary_add_carry_bound (bits_a bits_b : list Z) (i : nat) :
   bits_to_nat (take i bits_a) + bits_to_nat (take i bits_b) < 2^(i+1).
 Proof. Admitted.
 
-Lemma binary_sum_within_i32_bounds (bits_a bits_b : list Z) (n : Z) (partial_result : list Z) (y y0 : Z) (H1 : is_binary bits_a) (H2 : is_binary bits_b) (H15 : bits_a !! length partial_result = Some y) (H17 : bits_b !! length partial_result = Some y0) :
+Lemma binary_sum_within_i32_bounds (bits_a bits_b : list Z) (partial_result : list Z) (y y0 : Z) :
+  is_binary bits_a →
+  is_binary bits_b →
+  bits_a !! length partial_result = Some y →
+  bits_b !! length partial_result = Some y0 →
   y + y0 <= max_int i32.
 Proof. Admitted.
