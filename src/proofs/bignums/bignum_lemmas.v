40bbddaba75d894dff03d2ca83d2849e66559fe1
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-06-16 09:59:07 +0100

    refactor: remove redundant proof steps in bignum lemmas

--- src/proofs/bignums/bignum_lemmas.v
+++ src/proofs/bignums/bignum_lemmas.v
@@ -112,12 +112,6 @@ Proof.
       rewrite lookup_take in Hj; try lia.
       rewrite list_lookup_insert in Hj; try lia.
     -- injection Hj as Hj; subst x.
-      apply Z.rem_bound_pos; try lia.
-      assert (0 ≤ y + y0 + carry_val) by lia.
-      exact H.
-    + (* Case: j ≠ i_val *)
-      rewrite lookup_take in Hj; try lia.
-      rewrite list_lookup_insert_ne in Hj; auto.
     Show.
 Qed.
 
